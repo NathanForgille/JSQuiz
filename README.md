cf2445f284bc712258d136eb32c7b900328c73b2
Jean Ralphio <nateforgille@outlook.com>
2021-01-24 09:27:57 -0600

    initial commit. the files were created and some code has been developed. my main page as well as some styling.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JSQuiz
+
+This is a quiz application ran in the browser. The quiz was developed to test the users knowledge of JavaScript, as well as my own understanding of the language. 
+
---
+++ assets/script.js
@@ -0,0 +1,20 @@
+/*
+-I need to develop a welcome page that has instructions, a button to start the quiz, and has style that contains my content
+
+-When the quiz starts a header needs to appear that has the timer in it, and the high scores can be accessed as well
+
+-each time they answer a question the submit button takes them to the next question
+
+-if they also answered hte qyuestion incorrectly then time is removed from the timer
+
+-the quiz is over if the user answers all the questions or if the timer hits zero.
+
+-when the quiz is over, the user will be prompted to ener their initials to save their highscore
+
+-if the user does not qualify for a high score then the user will not be prompted to enter anything. Instead the will be taken to a page that encourages them to study and retake the quiz
+
+*/
+document.querySelector("#take-quiz").addEventListener("click", function(){
+
+}
+);
