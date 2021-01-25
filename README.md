37026730f415d1de0909853e581b849b0953a51d
Jean Ralphio <nateforgille@outlook.com>
2021-01-24 21:05:40 -0600

    I have a lot of updates to my JS, CSS, and HTML. The majority of the updates were to the JS. I created a lot of functionality, but found a big problem I could not fix in my user answer evaluation. I developed my read me to further explain the application and my problem

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 This is a quiz application ran in the browser. The quiz was developed to test the users knowledge of JavaScript, as well as my own understanding of the language. 
 
+The quiz starts with a home/instructions page. The user is prompted with a dynamic button. 
+
+The quiz starts and the score as well as the users progress is displayed at the top of the quiz. 
+
+The user is shown a form where they are able to select one answer. (I did not enter in the timer yet)
+
+They may submit their answer, and see their progress as they go. (if the user were to get the wrong answer the timer would dcrease in time)
+
+I ran into a problem in my code that I could not figure out the solution to. On lines 69-75 I wrote an if else statement to return the users answer in order to update the score, progression, and eventually the timer and high score elements. However no matter what I did to pull the users answer and equate it to the correct answer I always got an error or I got a user answer of null.
+
+This problem persisted, and without a solution for this problem I couldn't progress to make the rest of the application. I will most likely continue to work to resolve this error, and hopefully be able to resubmit, if allowed.
--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,70 @@
 -if the user does not qualify for a high score then the user will not be prompted to enter anything. Instead the will be taken to a page that encourages them to study and retake the quiz
 
 */
-document.querySelector("#take-quiz").addEventListener("click", function(){
+//Global Variables declared
+var master= [
+    {
+        question: "What is the purpose of JavaScript when building a site?",
+        answers: ["Functionality", "Styling", "Content", "Nothing. It's black magic."],
+        correctAnswer: "0"
+    },
+    {
+        question: "What does JavaScript have the ability to manipulate?",
+        answers: ["the DOM", "the url", "my glowing personality", "the stock market"],
+        correctAnswer: "0"
+    },
+    {
+        question: "What do you need for your JAvaScript to work?",
+        answers: ["HTML", "CSS", "JQuery", "none of the above"],
+        correctAnswer: "3"
+    }
+];
+var score = 0;
+var questionNumber = 0;
+var formIntro = document.getElementById("form-intro");
+var quizForm = document.getElementById("quiz-form");
+var questionText = document.getElementById("question-text");
+var answerSet = document.getElementById("answer-set");
 
+
+function generateAnswerSet(index){
+    var answerSetHtml = ``; 
+            for(let i = 0; i < master[index].answers.length; i++){
+            answerSetHtml += `
+            <label for="${i}">${master[index].answers[i]}</label>
+            <input name="${master[index].answers[i]}" id="${i}" type="radio" />
+            `;
+        } 
+        return answerSetHtml;
+}  
+
+function startQuiz(event){
+    event.preventDefault();
+    formIntro.setAttribute("class", "hidden");
+    quizForm.setAttribute("class", " ");
+    var firstQuestion = master[questionNumber].question;
+    questionText.textContent = firstQuestion;
+    var answerSetHtml = generateAnswerSet(questionNumber);
+        answerSet.innerHTML = answerSetHtml;
 }
-);
+
+
+function evalAnswer(event) {
+    // check if answer was correct
+    event.preventDefault();
+    var userAnswer = document.querySelector(`input[name="${master[questionNumber].answers[0]}"]:checked`.id);
+    if (parseInt(userAnswer) == master.correctAnswer) {
+        console.log("Yay!");
+        score += 1;
+        // do what happens if they get it right (ex. add to global score variable)
+    } else {
+        console.log("You got it wrong!")
+        // take time away, whatever
+    }
+    console.log(userAnswer);
+    questionNumber += 1;
+    questionText.innerText = master[questionNumber].question;
+    answerSet.innerHTML = generateAnswerSet(questionNumber);
+}
+quizForm.addEventListener("submit", evalAnswer)
+formIntro.addEventListener("submit", startQuiz)
